b9a31b1bdabf0369ba39f38af99ea9c1ad6177bb
unknown <hseoglan@naver.com>
2026-03-01 01:44:25 +0900

    3차 업데이트

--- src/components/FixedTopBtn.jsx
+++ src/components/FixedTopBtn.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import './styles/FixedTopBtn.scss'
+import { useSmoothScroll } from '../hooks/useSmoothScroll'
 
 const FixedTopBtn = () => {
+  const scrollTo = useSmoothScroll()
+
   return (
     <div className='FixedTopBtn'>
-      <button>위로 올라가는 버튼</button>
-      <a href="">
-          TCB
-      </a>
-      <p>
-          1:1 talk
-      </p>
+      <button
+        className='top-btn'
+        onClick={(e) => {
+          e.preventDefault()
+          scrollTo('hero')
+        }}>위로 올라가는 버튼</button>
+      <div className="talk-wrap">
+          <button className='talk-btn'>
+              <span class="icon"></span>
+              <a href="#" className='talk-btn'>
+                  TCB
+              </a>
+          </button>
+          <p>
+              1:1 talk
+          </p>
+      </div>
     </div>
   )
 }
