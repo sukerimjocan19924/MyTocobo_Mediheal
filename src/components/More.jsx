0737cedb983247d7fb85788e6d4bafc9b731c1ee
unknown <hseoglan@naver.com>
2026-03-10 21:02:33 +0900

    5차 업데이트-beyond-more

--- src/components/More.jsx
+++ src/components/More.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import "./styles/More.scss"
+import { more } from '../util/more'
 
 const More = () => {
+  const item = more[0];
+
   return (
-    <div>More</div>
+    <div className='inner more-inner' style={{backgroundImage:`url(${item.image.desktop})`}}>
+      <div className="txt-wrap">
+        <div className="text-wrap">
+          <div className="title">
+            {item.title}
+          </div>
+          <div className="subtitle">
+            {item.subtitle}
+          </div>
+        </div>
+
+        <div className="btn-wrap">
+          <a href={item.moreHref} className='more-btn'>
+            {item.btn}
+          </a>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/util/more.js
@@ -0,0 +1,15 @@
+const img = (path) => `/img/${path}`;
+
+// more용 데이터
+export const more = [
+  {
+    title: "MEDIHEAL",
+    subtitle: "모든 우리를 연구합니다.",
+    btn: "MORE",
+    moreHref: "/more/1",
+    image: {
+      desktop: img("more_img_d.png"),
+    },
+    alt: "forest-lake-background_img",
+  }
+];
