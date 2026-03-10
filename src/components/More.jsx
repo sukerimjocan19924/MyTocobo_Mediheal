1ba348c0ef12195681441fac9a2f3d349c1f9867
unknown <hseoglan@naver.com>
2026-03-10 21:13:40 +0900

    5차 업데이트-beyond-more 수정

--- src/components/More.jsx
+++ src/components/More.jsx
@@ -6,21 +6,23 @@ const More = () => {
   const item = more[0];
 
   return (
-    <div className='inner more-inner' style={{backgroundImage:`url(${item.image.desktop})`}}>
-      <div className="txt-wrap">
-        <div className="text-wrap">
-          <div className="title">
-            {item.title}
+    <div className='inner more-inner'>
+      <div className='more-bg' style={{backgroundImage:`url(${item.image.desktop})`}}>
+        <div className="txt-wrap">
+          <div className="text-wrap">
+            <div className="title">
+              {item.title}
+            </div>
+            <div className="subtitle">
+              {item.subtitle}
+            </div>
           </div>
-          <div className="subtitle">
-            {item.subtitle}
-          </div>
-        </div>
 
-        <div className="btn-wrap">
-          <a href={item.moreHref} className='more-btn'>
-            {item.btn}
-          </a>
+          <div className="btn-wrap">
+            <a href={item.moreHref} className='more-btn'>
+              {item.btn}
+            </a>
+          </div>
         </div>
       </div>
     </div>
